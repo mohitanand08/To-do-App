490f6764f0f71cd0ec8cafa479de9bc15e5b35d9
Mohit Anand <mohitanand0810@gmail.com>
2019-12-25 00:54:44 +0530

    Designed To-do List : Css file added to assets file and favicon added

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 const express = require('express');
+const path = require('path');
 const port = 8000;
 const app = express();
+// middleware
+app.use(express.urlencoded());
+app.use(express.static('assets'));
 
-
+// Template Engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+// To-do list
+var toDoList = [
+    {
+        description: 'get veg',
+        category: 'Personal',
+        date: '08/09/2019'
+    },
+    {
+        description: 'Do english Home work',
+        category: 'School',
+        date: '18/09/2019'
+    }
+]
+// Controller
+app.get('/', function(req, res){
+    return res.render('todo', {
+        title: 'To-do App',
+        to_do_list: toDoList
+    });
+});
 
 app.listen(port, function(err){
     if (err) {console.log(`Error in running the server : ERROR = ${err}`);
