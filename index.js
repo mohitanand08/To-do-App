7c614323f387e9b4d9cf1a3f035ee14d3ebaa048
Mohit Anand <mohitanand0810@gmail.com>
2019-12-27 20:04:49 +0530

    added checkBox feature

--- index.js
+++ index.js
@@ -12,61 +12,53 @@ app.use(express.static('assets'));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded());
-// To-do list
-var toDoList = [
-    {
-        description: 'get veg',
-        category: 'Personal',
-        date: '08/09/2019'
-    },
-    {
-        description: 'Do english Home work',
-        category: 'School',
-        date: '18/09/2019'
-    }
-]
 // Controller
-app.get('/', function(req, res){
-    Todo.find({}, function(err, todos){
-        if(err){
-            console.log('Error in fetching data from db');
-            return;
-        }
-        return res.render('todo', {
-            title: 'To-do App',
-            to_do_list: todos
-        });
-    })
+app.get('/', function(req, res) {
+	Todo.find({}, function(err, todos) {
+		if (err) {
+			console.log('Error in fetching data from db');
+			return;
+		}
+		return res.render('todo', {
+			title: 'To-do App',
+			to_do_list: todos
+		});
+	});
 });
 // Add Task to the To-do-List
-app.post('/create-to-do-list',function(req, res){
-    Todo.create({
-        description: req.body.description,
-        category: req.body.category,
-        date: req.body.date
-    }, function(err, newTodo){
-        if(err){
-            console.log('Error in creating a To-do Task');
-            return;
-        }
-        res.redirect('back')     
-    });
+app.post('/create-to-do-list', function(req, res) {
+	Todo.create(
+		{
+			description: req.body.description,
+			category: req.body.category,
+			date: req.body.date
+		},
+		function(err, newTodo) {
+			if (err) {
+				console.log('Error in creating a To-do Task');
+				return;
+			}
+			res.redirect('back');
+		}
+	);
 });
 // Delete Task from the to-do-list
-app.get('/delete-task', function(req, res){
-    // get the id from the query in the ul
-    let id = req.query.id;
-    // find the todo in the database using id and delete it
-    Todo.findByIdAndDelete(id, function(err){
-        if(err){ console.log('Error in deleting an object from the database');
-        return;
-    }
-    return res.redirect('back');
-    });
+app.get('/delete-task', function(req, res) {
+	// get the id from the query in the ul
+	let id = req.query.id;
+	// find the todo in the database using id and delete it
+	Todo.findByIdAndDelete(id, function(err) {
+		if (err) {
+			console.log('Error in deleting an object from the database');
+			return;
+		}
+		return res.redirect('back');
+	});
 });
 
-app.listen(port, function(err){
-    if (err) {console.log(`Error in running the server : ERROR = ${err}`);
-    }
-    console.log(`Server is running in PORT : ${port}`);
+app.listen(port, function(err) {
+	if (err) {
+		console.log(`Error in running the server : ERROR = ${err}`);
+	}
+	console.log(`Server is running in PORT : ${port}`);
 });
