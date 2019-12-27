6b98cb3258f8ef66eac70d02a79e2c1c5d09b7b8
Mohit Anand <mohitanand0810@gmail.com>
2019-12-28 00:26:36 +0530

    added comments to the code

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Adding the modules
 const express = require('express');
 const path = require('path');
 const port = 8000;
--- models/todo.js
+++ models/todo.js
@@ -1,19 +1,20 @@
+// Mongoose module added
 const mongoose = require('mongoose');
-
+// Schema Structure
 const todoSchema = new mongoose.Schema({
-    description: {
-        type: String,
-        required: true
-    },
-    category: {
-        type: String,
-        required: true
-    },
-    date: {
-        type: String,
-        required: true
-    }
-})
-// Collection 
+	description: {
+		type: String,
+		required: true
+	},
+	category: {
+		type: String,
+		required: true
+	},
+	date: {
+		type: String,
+		required: true
+	}
+});
+// Collection
 const Todo = mongoose.model('Todo', todoSchema);
 module.exports = Todo;
